3592b80e98a84a183d42c0b9f593f928e804dc64
clemes-pi <carolina.lemes@piconsulting.com.ar>
2025-08-06 16:15:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-import asyncio
-from functions import procesar_excel, WebSocketClient, WS_URL_REPHRASE, WS_URL_SIMILARITY, WS_AUTH_TOKEN
+from functions import procesar_excel_http
 
 if __name__ == "__main__":
     archivo_excel = "archivo.xlsx"
-
-    async def main():
-        ws_rephrase = WebSocketClient(WS_URL_REPHRASE, WS_AUTH_TOKEN)
-        ws_similarity = WebSocketClient(WS_URL_SIMILARITY, WS_AUTH_TOKEN)
-        await ws_rephrase.connect()
-        await ws_similarity.connect()
-        try:
-            await procesar_excel(archivo_excel, ws_rephrase, ws_similarity)
-        finally:
-            await ws_rephrase.close()
-            await ws_similarity.close()
-
-    asyncio.run(main())
+    procesar_excel_http(archivo_excel)
